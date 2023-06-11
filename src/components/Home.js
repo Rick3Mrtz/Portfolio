f3a57188c00d4ba483f6ad4e9a131dd0bad7ef47
Rick3Mrtz <Rick.mrtz3@gmail.com>
2023-06-11 04:49:55 -0500

    added logo and content to landing page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,27 @@ import '../styles/Home.css'
 function Home () {
     return (
         <>
-        <div className="p-[20rem] bg-[#151515] mx-20 rounded-3xl">
-            <div className="">
+        <div className=" h-min bg-[#151515] rounded-[40px] flex flex-column flex-nowrap justify-center mx-20 p-20">
+            <div className="content flex-col h-min flex-nowrap justify-start gap-8 items-center mx-40">
+
+                <div className="title text-[60px]  text-center font-extrabold leading-none text-[#fff] space-y-8">
+                    Hello, Nice to Meet You<br></br> My Name is Rick
+                    <p className="subtext text-[18px] text-[#54545d] font-sans font-normal ">
+                    Front End Web Dev who also just wants to take up some room to see how all of this looks like. Also please Lord help me find a job eventually or else I will sell all of my Pokemon cards.
+                    </p>
+                </div>
+
+                <div className="flex justify-center items-center py-8 text-[#fff] font-bold text-xl">
+
+                <button className="bg-gradient-to-b from-orange-300 to-orange-600 px-10 py-3 rounded-[40px]">
+                    Contact Me
+                </button>
+
+                </div>
+
+                <div>
+
+                </div>
         
             </div>
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import '../styles/Navbar.css'
+import { FaReact } from 'react-icons/fa'
+
 
 function Navbar () {
     return (
@@ -9,7 +11,8 @@ function Navbar () {
             <ul className=' flex flex-row flex-nowrap justify-center items-center overflow-hidden'>
               <li className='px-10'><a href="#">Home</a></li>
               <li className='px-10'><a href="#">About</a></li>
-              <a href="#" className="logo px-10">LOGO</a>
+              <FaReact className='logo animate-spin text-orange-500' size={50} />
+
               <li className='px-10'><a href="#">Education</a></li>
               <li className='px-10'><a href="#">Contact</a></li>
             </ul>
