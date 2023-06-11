aec54ff8599919e89a552c01993ab346378f55c2
Rick3Mrtz <Rick.mrtz3@gmail.com>
2023-06-11 02:14:36 -0500

    added Home.js and chose landing page background color

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import '../styles/Home.css'
+
+function Home () {
+    return (
+        <>
+        <div className="p-[20rem] bg-[#151515] mx-20 rounded-3xl">
+            <div className="">
+        
+            </div>
+
+        </div>
+        </>
+    )
+}
+
+export default Home;
