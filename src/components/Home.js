7bb343f7619245cdd8ff1be727d2de39d6d10189
Rick3Mrtz <Rick.mrtz3@gmail.com>
2023-06-13 05:03:51 -0500

    fixed animation speed for React logo, added pictures on landing page, made content and text all responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,18 @@
 import React from "react";
 import '../styles/Home.css'
+import Laptop from '../assets/port-laptop2.png'
+import Phone from '../assets/port-phone2.png'
 
 function Home () {
     return (
         <>
-        <div className=" h-min bg-[#151515] rounded-[40px] flex flex-column flex-nowrap justify-center mx-20 p-20">
-            <div className="content flex-col h-min flex-nowrap justify-start gap-8 items-center mx-40">
+        <div className=" h-min bg-[#151515] rounded-[40px] flex flex-column flex-nowrap justify-center mx-16 py-20 px-6 md:px-10 lg:px-40 xl:px-56">
+            <div className="content flex-col h-min flex-nowrap justify-start gap-8 items-center">
 
-                <div className="title text-[60px]  text-center font-extrabold leading-none text-[#fff] space-y-8">
+                <div className="title text-center font-extrabold leading-none text-[#fff] space-y-8">
                     Hello, Nice to Meet You<br></br> My Name is Rick
                     <p className="subtext text-[18px] text-[#54545d] font-sans font-normal ">
-                    Front End Web Dev who also just wants to take up some room to see how all of this looks like. Also please Lord help me find a job eventually or else I will sell all of my Pokemon cards.
+                    Front End Web Dev who also needs to find some placeholders for this text. Idk here is sports stuff, lets go yankees! Avid PokemonGo player and DragonBall Z Stan. Myles and Joshy for life, and last but not least lets go Cowboys.
                     </p>
                 </div>
 
@@ -22,9 +24,11 @@ function Home () {
 
                 </div>
 
-                <div>
+        <div class="flex justify-center gap-4 lg:gap-16 pt-2">
+                <img src={Laptop} alt="Laptop" class="w-[50%] lg:w-[65%] rounded-3xl " />
+                <img src={Phone} alt="Phone" class="w-[50%] lg:w-[65%] rounded-3xl " />
+        </div>
 
-                </div>
         
             </div>
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,29 @@
 import React from "react";
-import '../styles/Navbar.css'
-import { FaReact } from 'react-icons/fa'
+import "../styles/Navbar.css";
+import { FaReact } from "react-icons/fa";
 
-
-function Navbar () {
-    return (
-        <>
-        <div className="navbar my-8 mx-auto">
-          
-            <ul className=' flex flex-row flex-nowrap justify-center items-center overflow-hidden'>
-              <li className='px-10'><a href="#">Home</a></li>
-              <li className='px-10'><a href="#">About</a></li>
-              <FaReact className='logo animate-spin text-orange-500' size={50} />
-
-              <li className='px-10'><a href="#">Education</a></li>
-              <li className='px-10'><a href="#">Contact</a></li>
-            </ul>
-          
-        </div>
-      </>
-    )
+function Navbar() {
+  return (
+    <>
+      <div className="navbar my-8 mx-auto">
+        <ul className="flex flex-row flex-nowrap justify-center items-center overflow-hidden">
+          <li className="px-10">
+            <a href="#" className="nav-link">Home</a>
+          </li>
+          <li className="px-10">
+            <a href="#" className="nav-link">About</a>
+          </li>
+          <FaReact className="logo animate-spin text-orange-500" size={50} />
+          <li className="px-10">
+            <a href="#" className="nav-link">Education</a>
+          </li>
+          <li className="px-10">
+            <a href="#" className="nav-link">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default Navbar;
