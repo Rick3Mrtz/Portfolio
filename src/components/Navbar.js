70c3588e10ecdd51562851eb395c53a7b9ee1ffd
Rick3Mrtz <Rick.mrtz3@gmail.com>
2023-06-29 15:58:32 -0500

    added hamburger menu for small screens, useState, functionality works

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Navbar.css";
 import { FaReact } from "react-icons/fa";
 import { Link } from 'react-scroll';
-
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 function Navbar() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const handleMobileMenuToggle = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <>
-      <div className="navbar my-8 mx-auto">
-        <ul className="flex flex-row flex-nowrap justify-center items-center overflow-hidden">
+      <div className="navbar my-8 mx-auto w-full">
+        {/* Hamburger menu button */}
+
+        <div className="bg-white fixed py-8 top-0 right-0 w-full z-10 lg:hidden md:hidden">
+          <div
+            className="hamburger-btn"
+            onClick={handleMobileMenuToggle}>
+            <button>
+              <GiHamburgerMenu size={40} />
+            </button>
+          </div>
+        </div>
+
+
+        {/* Nav Links */}
+        <ul
+          className={`flex flex-row flex-nowrap justify-center items-center overflow-hidden ${isMobileMenuOpen ? "mobile-menu-open" : ""
+            }`}
+        >
           <li className="px-10">
 
             <button className="nav-link cursor-pointer">
@@ -65,5 +88,6 @@ function Navbar() {
     </>
   );
 }
+
 
 export default Navbar;
